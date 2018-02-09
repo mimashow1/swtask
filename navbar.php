b61cb1bd2f3dd7c04cb658657f149cbdfb81b898
Danik <dan1617laz@gmail.com>
2018-02-08 21:34:28 -0800

    creating file with navbar code

---
+++ navbar.php
@@ -0,0 +1,51 @@
+<?php
+    echo '<div class="hidden-xs">
+    <nav class="navbar navbar-inverse">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+            </div>
+            <div class="collapse navbar-collapse" id="myNavbar">
+                <ul class="nav navbar-nav">
+                    <li><a href="#"><i class="fas fa-user"></i> Account</a></li>
+                    <li><a href="#"><i class="fas fa-newspaper"></i> News</a></li>
+                    <li><a href="#"><i class="fas fa-bell"></i> Notifications</a></li>
+                    <li><a href="#"><i class="fas fa-bookmark"></i> Saved</a></li>
+                    <li class="active"><a href="#"><i class="fas fa-home"></i> Home</a></li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+                    <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</div>
+<div class="visible-xs">
+
+    <div class="downMenu">
+
+        <div class="menu">
+            <div class="element">
+                <li><a href="#"><i class="fas fa-user" id="icon"></i></a></li>
+            </div>
+            <div class="element">
+                <li><a href="#"><i class="fas fa-newspaper" id="icon"></i></a></li>
+            </div>
+            <div class="element">
+                <li><a href="#"><i class="fas fa-bell" id="icon"></i></a></li>
+            </div>
+            <div class="element">
+                <li><a href="#"><i class="fas fa-bookmark" id="icon"></i></a></li>
+            </div>
+            <div class="element">
+                <li><a href="#"><i class="fas fa-home" id="icon"></i></a></li>
+            </div>
+        </div>
+    </div>
+</div>';
+?>
