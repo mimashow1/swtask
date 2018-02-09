e45051763b6f54af59d43739f8a0510df619815c
Danik <dan1617laz@gmail.com>
2018-02-08 22:22:49 -0800

    image and gradient

--- index.php
+++ index.php
@@ -9,56 +9,19 @@
     <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
 
     <link rel="stylesheet" type="text/css" href="css/navbar.css">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
     <title>Home</title>
 </head>
 <body>
-<div class="hidden-xs">
-    <nav class="navbar navbar-inverse">
-        <div class="container-fluid">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-            </div>
-            <div class="collapse navbar-collapse" id="myNavbar">
-                <ul class="nav navbar-nav">
-                    <li><a href="#"><i class="fas fa-user"></i> Account</a></li>
-                    <li><a href="#"><i class="fas fa-newspaper"></i> News</a></li>
-                    <li><a href="#"><i class="fas fa-bell"></i> Notifications</a></li>
-                    <li><a href="#"><i class="fas fa-bookmark"></i> Saved</a></li>
-                    <li class="active"><a href="#"><i class="fas fa-home"></i> Home</a></li>
-                </ul>
-                <ul class="nav navbar-nav navbar-right">
-                    <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-                    <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-</div>
-<div class="visible-xs">
-    <div class="downMenu">
-        <div class="menu">
-            <div class="element">
-                <li><a href="#"><i class="fas fa-user" id="icon"></i></a></li>
-            </div>
-            <div class="element">
-                <li><a href="#"><i class="fas fa-newspaper" id="icon"></i></a></li>
-            </div>
-            <div class="element">
-                <li><a href="#"><i class="fas fa-bell" id="icon"></i></a></li>
-            </div>
-            <div class="element">
-                <li><a href="#"><i class="fas fa-bookmark" id="icon"></i></a></li>
-            </div>
-            <div class="element">
-                <li><a href="#"><i class="fas fa-home" id="icon"></i></a></li>
+    <?php include 'navbar.php'; ?>
+    <div class="image">
+        <div class=" parent">
+            <div class="child">
+
             </div>
         </div>
     </div>
-</div>
+
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </body>
